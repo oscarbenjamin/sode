9ae1ee778b6172543b1ec2d0d51480b28d77a9be
Oscar Benjamin <oscar.j.benjamin@gmail.com>
2011-11-03 17:47:23 +0000

    Added ziggurat - needs tidying

--- profile/weiner.c
+++ profile/weiner.c
@@ -39,6 +39,19 @@
     #define TWOPI 6.2831853071796
 #endif
 
+/* Sort this out. Don't want these macros. */
+float nfix(void);
+#define SHR3 (jz=jsr, jsr^=(jsr<<13), jsr^=(jsr>>17), jsr^=(jsr<<5),jz+jsr)
+#define UNI (.5 + (signed) SHR3*.2328306e-9)
+#define IUNI SHR3
+#define RNOR (hz=SHR3, iz=hz&127, (fabs(hz)<kn[iz])? hz*wn[iz] : nfix())
+#define REXP (jz=SHR3, iz=jz&255, (    jz <ke[iz])? jz*we[iz] : efix())
+static unsigned long jz,jsr=123456789;
+static long hz;
+static unsigned long iz, kn[128], ke[256];
+static float wn[128],fn[128], we[256],fe[256];
+
+
 /* Main program routines */
 int print_help(char* argv[], char* msg);
 int print_weiner();
@@ -62,8 +75,10 @@ int main(int argc, char *argv[])
     /* Initialise random seed and prepare for rng generation */
     init_rngs();
 
-    for (i=0; i < 1000000; i++)
-        printf("%.15f\n", randn_boxmuller());
+    for (i=0; i < 100000000; i++) {
+        randn_boxmuller();
+    }
+        //printf("%.15f\n", RNOR);
     return 0;
 
     if(argc == 1)
@@ -192,6 +207,7 @@ rint_type xorshift(void) {
  * Set up random seed and initialise rngs.
  */
 
+void ziggurat_seed(unsigned long jsrseed);
 void init_rngs(void) {
     /* Initialise with system time and iterate a few times */
     srand(initial_seed());
@@ -201,6 +217,7 @@ void init_rngs(void) {
 
     /* Initialise xorshift array */
     xorshift_seed();
+    ziggurat_seed(rand());
 }
 
 /*
@@ -247,4 +264,78 @@ double randn_boxmuller() {
     }
 
     return u[i];
+}
+
+/*
+ * Ziggurat algorithm
+ *
+ * From
+ * http://www.jstatsoft.org/v05/i08/supp/1
+ *
+ * Combine the code below with the main program in which you want normal or
+ * exponential variates.   Then use of RNOR in any expression will provide a
+ * standard normal variate with mean zero, variance 1, while use of REXP in
+ * any expression will provide an exponential variate with density
+ * exp(-x),x>0.  Before using RNOR or REXP in your main, insert a command such
+ * as ziggurat_seed(86947731 ); with your own choice of seed value>0, rather than
+ * 86947731.  (If you do not invoke ziggurat_seed(...) you will get all zeros for
+ * RNOR and REXP.) For details of the method, see Marsaglia and Tsang, "The
+ * ziggurat method for generating random variables", Journ. Statistical
+ * Software.
+ */
+
+/* nfix() generates variates from the residue when rejection in RNOR occurs. */
+
+float nfix(void)
+{
+    const float r = 3.442620f;     /* The start of the right tail */
+    static float x, y;
+    for(;;)
+    {
+        x=hz*wn[iz];      /* iz==0, handles the base strip */
+        if(iz==0)
+        {
+            do{
+                x=-log(UNI)*0.2904764; y=-log(UNI);
+            } while(y+y<x*x);   /* .2904764 is 1/r */
+            return (hz>0)? r+x : -r-x;
+        }
+        /* iz>0, handle the wedges of other strips */
+        if( fn[iz]+UNI*(fn[iz-1]-fn[iz]) < exp(-.5*x*x) ) return x;
+
+        /* initiate, try to exit for(;;) for loop*/
+        hz=SHR3;
+        iz=hz&127;
+        if(fabs(hz)<kn[iz]) return (hz*wn[iz]);
+    }
+}
+
+/*--------This procedure sets the seed and creates the tables------*/
+
+void ziggurat_seed(unsigned long jsrseed)
+{
+    const double m1 = 2147483648.0, m2 = 4294967296.;
+    double dn=3.442619855899,tn=dn,vn=9.91256303526217e-3, q;
+    double de=7.697117470131487, te=de, ve=3.949659822581572e-3;
+    int i;
+    jsr^=jsrseed;
+
+    /* Set up tables for RNOR */
+    q=vn/exp(-.5*dn*dn);
+    kn[0]=(dn/q)*m1;
+    kn[1]=0;
+
+    wn[0]=q/m1;
+    wn[127]=dn/m1;
+
+    fn[0]=1.;
+    fn[127]=exp(-.5*dn*dn);
+
+    for(i=126;i>=1;i--) {
+        dn=sqrt(-2.*log(vn/dn+exp(-.5*dn*dn)));
+        kn[i+1]=(dn/tn)*m1;
+        tn=dn;
+        fn[i]=exp(-.5*dn*dn);
+        wn[i]=dn/m1;
+    }
 }
