3ae4f7dc49f54813b6c103e824b59534ae251e49
Oscar Benjamin <oscar.j.benjamin@gmail.com>
2011-11-04 16:27:44 +0000

    Begun arg parsing for weiner.c

--- profile/weiner.c
+++ profile/weiner.c
@@ -8,46 +8,70 @@
 #include <stdio.h>
 #include <math.h>
 #include <time.h>
+#include <string.h>
 
 /* For generating standard normals. */
 #include "randnorm.h"
 
-/* Main program routines */
-int print_help(char* argv[], char* msg);
-int print_weiner();
+/* Enumerate the possible systems we can define */
+typedef enum {LINEAR2D, LINEARMULT, SINMULT, TANMULT, WEINER, WEINER2} SysType;
+
+/* Struct to hold the data returned by parse_args */
+#define INVALID_ARGUMENTS 0
+#define GOOD_ARGUMENTS 1
+typedef struct InputOptionsStruct {
+    SysType systype;
+    double t1;
+    double t2;
+    double dtmax;
+    double dtout;
+} InputOptions;
+
+/* Function used to process the command line args. */
+int parse_args(int argc, char *argv[], InputOptions *opts);
 
 int main(int argc, char *argv[])
 {
-    int systype;
+    InputOptions opts;
 
     /* Initialise random seed and prepare for rng generation */
     randnorm_seed(RANDNORM_SEED_PID_TIME);
 
-    if(argc == 1)
-        return print_help(argv, NULL);
-    else {
-        if(!sscanf(argv[1], "%i", &systype))
-            return print_help(argv, "Cannot parse SYSNUM\n");
-        switch(systype){
-            case 0:
-                return print_weiner();
-            default:
-                return print_help(argv, "Invalid SYSNUM\n");
-        }
-    }
-
-    return 0;
+    if(parse_args(argc, argv, &opts) == INVALID_ARGUMENTS)
+        return 1;
+    else
+        return 0;
 }
 
-int print_help(char *argv[], char *errmsg)
+int print_help(int argc, char *argv[], char *errmsg)
 {
     if(errmsg != NULL)
-        fprintf(stderr, errmsg);
+        fprintf(stderr, "%s\n", errmsg);
     printf("usage: %s SYSNUM\n", argv[0]);
     printf("\n");
     printf("Numerically solve the stochastic ordinary differential\n");
     printf("equation representing a Weiner process.\n");
     return 0;
+}
+
+int invalid_arguments(int argc, char *argv[], char *errmsg) {
+    print_help(argc, argv, errmsg);
+    return INVALID_ARGUMENTS;
+}
+
+/* Used by main to process the input arguments */
+int parse_args(int argc, char *argv[], InputOptions *opts) {
+    /* Check the number of arguments first */
+    if(argc != 1 + 1)
+        return print_help(argc, argv, "Wrong number of arguments");
+
+    /* Check the arguments in order */
+    argv++;
+    if(!strcmp(*argv, "weiner"))
+        opts->systype = WEINER;
+    else
+        return invalid_arguments(argc, argv, "Unrecognised sysname");
+    return GOOD_ARGUMENTS;
 }
 
 void sprint_time(char *timestr, size_t max) {
@@ -63,8 +87,8 @@ int print_weiner()
     double x1=0;
     double alpha=0, beta=1;
     double t1=0;
-    double t2=1;
-    double dt=0.01;
+    double t2=100;
+    double dt=0.0001;
     double sqrtdt=sqrt(dt);
 
     int nsteps=(int) ceil((t2 - t1) / dt);
