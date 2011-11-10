3b2bdbe80e0ad53dd3891482ac07413dd44ff108
Oscar Benjamin <oscar.j.benjamin@gmail.com>
2011-11-10 17:00:48 +0000

    Added cyexamples to time.sh script

--- sode/examples/time.sh
+++ sode/examples/time.sh
@@ -6,9 +6,10 @@ set -o errexit
 
 # Integration variables
 t1=0
-t2=10
-dtmax=0.0001
-dtout=0.01
+t2=100
+t2l=$(($t2*100))
+dtmax=0.001
+dtout=0.1
 
 echo
 echo Performance comparison between pure python and c implementation of the
@@ -16,22 +17,37 @@ echo examples here.
 echo
 
 for sys in\
+    weiner\
+    weiner2\
     lin2d\
     linmult\
     sinmult\
-    tanmult\
-    weiner\
-    weiner2
+    tanmult
     do
+        echo
         echo ----------- Example $sys -----------
-        echo ----- In Pure python
+        echo
+        echo ----- In Pure python $t2 secs
         time ./pyexamples.py solve --system-type=$sys --t1=$t1 --t2=$t2\
                                    --dtmax=$dtmax --dtout=$dtout > .tmppy
         echo
-        echo ----- In Pure c
+        echo ----- In Cython      $t2 secs
+        time ./cyexamples.py solve --system-type=$sys --t1=$t1 --t2=$t2\
+                                   --dtmax=$dtmax --dtout=$dtout > .tmpcy
+        echo
+        echo ----- In Pure c      $t2 secs
         time ./cexamples.exe $sys $t1 $t2 $dtmax $dtout > .tmpc
         echo
+
         echo
+        echo ----- In Cython      $t2l secs
+        time ./cyexamples.py solve --system-type=$sys --t1=$t1 --t2=$t2l\
+                                   --dtmax=$dtmax --dtout=$dtout > .tmpcy
+        echo
+        echo ----- In Pure c      $t2l secs
+        time ./cexamples.exe $sys $t1 $t2l $dtmax $dtout > .tmpc
+        echo
+
 done
 
-rm .tmpc .tmppy
+rm .tmpc .tmppy .tmpcy
